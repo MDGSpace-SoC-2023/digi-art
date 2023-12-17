1591ba1fa22de422330300e778fc30ffd9f5abc5
KARTHIK SATHISH <karthiksathishjeemain@gmail.com>
2023-12-18 04:27:25 +0530

    Started frontend portion by creating home page

--- SmartContracts/scripts/deploy.js
+++ SmartContracts/scripts/deploy.js
@@ -1,11 +1,7 @@
 
 const { ethers }= require("hardhat");
-// const hre = require("hardhat");
-// const ethers = hre.ethers;
-
 async function main(){
   const [deployer]= await ethers.getSigners();
-
   console.log("Balance of deployer is :" + await deployer.getBalance())
   console.log(`address of deployer is ${deployer.address}`)
   const NFT = await ethers.getContractFactory("NFT")
