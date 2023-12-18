cfc6ce89f029cdc0b71e1047d4398c867400a63b
KARTHIK SATHISH <karthiksathishjeemain@gmail.com>
2023-12-18 21:26:44 +0530

    Added Home.js,modified App.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,4 +2,8 @@ require("@nomiclabs/hardhat-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  paths: {
+    sources: "./src/SmartContracts/contracts",
+    tests: "./src/SmartContract/test"
+  }
 };
---
+++ src/SmartContracts/scripts/deploy.js
@@ -0,0 +1,40 @@
+
+const { ethers }= require("hardhat");
+async function main(){
+  const [deployer]= await ethers.getSigners();
+  console.log("Balance of deployer is :" + await deployer.getBalance())
+  console.log(`address of deployer is ${deployer.address}`)
+  const NFT = await ethers.getContractFactory("NFT")
+  const MarketPlace = await ethers.getContractFactory("MarketPlace")
+  const nft =  await NFT.deploy()
+  const marketPlace = await MarketPlace.deploy(5)
+  saveFrontendFiles(nft,"NFT")
+  saveFrontendFiles(marketPlace,"MarketPlace")
+}
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../../frontend/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+}
+// main()
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
