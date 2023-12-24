d18c89b517ff8e8b035175795a176b54ac023fec
KARTHIK SATHISH <karthiksathishjeemain@gmail.com>
2023-12-24 21:28:08 +0530

    Clean the commented  section in Create Component

--- src/Frontend/components/Create.js
+++ src/Frontend/components/Create.js
@@ -2,29 +2,13 @@ import axios from 'axios';
 import { useState } from 'react'
 import { ethers } from "ethers"
 import { Row, Form, Button } from 'react-bootstrap'
-// import { create as ipfsHttpClient } from 'ipfs-http-client'
-// const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
 const Create = ({ marketplace, nft }) => {
   const [fileImg, setFile] = useState(null);
   const [name, setName] = useState("")
   const [desc, setDescription] = useState("")
   const [price, setPrice] = useState("")
-  // const uploadToIPFS = async (event) => {
-  //   event.preventDefault()
-  //   const file = event.target.files[0]
-  //   if (typeof file !== 'undefined') {
-  //     try {
-  //       const result = await client.add(file)
-  //       console.log(result)
-  //       setImage(`https://ipfs.infura.io/ipfs/${result.path}`)
-  //     } catch (error){
-  //       console.log("ipfs image upload error: ", error)
-  //     }
-  //   }
-  // }
-
-  ////////////////////////////////////////////////////////
+ 
 
 
 
@@ -47,8 +31,7 @@ const Create = ({ marketplace, nft }) => {
         },
       });
 
-      // https://gateway.pinata.cloud/ipfs/QmZ6iZAhazHyakzynC4sxZ6r6cikJmS69mZaCoyburKuq
-
+    
 
       const tokenURI = `https://gateway.pinata.cloud/ipfs/${resJSON.data.IpfsHash}`;
       console.log("Token URI", tokenURI);
@@ -102,17 +85,8 @@ const Create = ({ marketplace, nft }) => {
     }
   }
 
-  ////////////////////////////////////////////////////////
-  // const createNFT = async () => {
-  //   if (!image || !price || !name || !description) return
-  //   try{
-  //     sendJSONtoIPFS(image)
-  //     // const result = await client.add(JSON.stringify({image, price, name, description}))
-  //     // mintThenList(result)
-  //   } catch(error) {
-  //     console.log("ipfs uri upload error: ", error)
-  //   }
-  // }
+
+
   const mintThenList = async (uri) => {
     // const uri = `https://ipfs.infura.io/ipfs/${result.path}`
     // mint nft 
